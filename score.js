f630576ac4a94bb3bbed90ca4f3337424d3dd9ea
govardhan <govardhan.n@people10.com>
2018-12-21 16:17:05 +0530

    Changes to score.js

--- score.js
+++ score.js
@@ -8,23 +8,32 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 
 // Write code here to analyze stuff
 function runAnalysis() {
-	const testSize = 50;
-	const [testSet, trainingSet] = splitDataset(outputs, testSize);
+	const testSetSize = 100;
+	const k = 10;
 
-	_.range(1, 15).forEach(k => {
+	_.range(0, 3).forEach(feature => {
+		//feature === 0, feature === 1, feature === 2
+		const data = _.map(outputs, row => [row[feature], _.last(row)]);
+		const [testSet, trainingSet] = splitDataset(minMax(data, 1), testSetSize);
+
 		const accuracy = _.chain(testSet)
-			.filter(testPoint => knn(trainingSet, testPoint[0], k) === testPoint[3])
+			.filter(testPoint => knn(trainingSet, _.initial(testPoint), k) === _.last(testPoint))
 			.size()
-			.divide(testSize)
+			.divide(testSetSize)
 			.value()
-		console.log('For k of ', k, ' Accuracy ', accuracy);
+		console.log('For feature of ', feature, ' Accuracy ', accuracy);
 	});
 };
 
 // KNN Algorithm
 function knn(data, point, k) {
+	//Point has only 3 values i.e [dropPosition, bounciness, ballSize]
 	return _.chain(data)
-		.map(row => [distance(row[0], point), row[3]])
+		.map(row => {
+			return [
+				distance(_.initial(row), point), _.last(row)
+			];
+		})
 		.sortBy(row => row[0])
 		.slice(0, k)
 		.countBy(row => row[1])
@@ -33,12 +42,19 @@ function knn(data, point, k) {
 		.last()
 		.first()
 		.parseInt()
-		.value()
+		.value();
 };
 
 
 function distance(pointA, pointB) {
-	return Math.abs(pointA - pointB)
+	//PointA from trainingSet = [dropPosition, bounciness, ballSize]
+	//PointB from testSet = [dropPosition, bounciness, ballSize]
+
+	return _.chain(pointA)
+		.zip(pointB)
+		.map(([a, b]) => (a - b) ** 2)
+		.sum()
+		.value() ** 0.5;
 };
 
 function splitDataset(data, testCount) {
@@ -49,3 +65,20 @@ function splitDataset(data, testCount) {
 
 	return [testSet, trainingSet];
 };
+
+
+function minMax(data, featureCount) {
+	const clonedData = _.cloneDeep(data);
+
+	for (let i = 0; i < featureCount; i++) {
+		//column extraction
+		const column = clonedData.map(row => row[i]);
+		const min = _.min(column);
+		const max = _.max(column);
+
+		for (let j = 0; j < clonedData.length; j++) {
+			clonedData[j][i] = (clonedData[j][i] - min) / (max - min);
+		}
+	};
+	return clonedData;
+}
